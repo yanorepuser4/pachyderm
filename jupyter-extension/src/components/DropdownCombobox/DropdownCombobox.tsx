888270fcee6a2b18cd49b542ebe9df38da110059
Emma Adams <emma.adams@hpe.com>
2024-02-29 08:41:32 +0000

    Implement DropdownCombobox

---
+++ jupyter-extension/src/components/DropdownCombobox/DropdownCombobox.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import {useCombobox} from 'downshift'
+
+export type DropdownComboboxProps = {
+  initialSelectedItem: string | null;
+  items: string[];
+  placeholder?: string;
+  onSelectedItemChange: (selectedItem: string) => void
+}
+
+export const DropdownCombobox: React.FC<DropdownComboboxProps> = ({initialSelectedItem, items, placeholder, onSelectedItemChange}) => {
+  const [inputItems, setInputItems] = React.useState(items)
+  const {
+    isOpen,
+    getMenuProps,
+    getInputProps,
+    highlightedIndex,
+    getItemProps,
+    selectedItem,
+    selectItem
+  } = useCombobox({
+    items: inputItems,
+    initialIsOpen: initialSelectedItem ? false : true,
+    initialSelectedItem: initialSelectedItem,
+    onInputValueChange: ({inputValue}) => {
+      setInputItems(
+        inputValue ? items.filter(item =>
+          item.toLowerCase().startsWith(inputValue.toLowerCase()),
+        ) : items,
+      )
+    },
+    onIsOpenChange: ({isOpen, selectedItem}) => {
+      if (isOpen && selectedItem) {
+        selectItem(null)
+      }
+    },
+    onSelectedItemChange: ({selectedItem}) => {
+      if (!selectedItem) {
+        return;
+      }
+
+      onSelectedItemChange(selectedItem)
+    }
+  })
+
+  // TODO: Consider clearing the text on click
+  // TODO: Show the five most recents first potentially with a separator
+  // TODO: Post about this in #nbredesign channel. Potentially
+  return (
+    <div className='pachyderm-DropdownCombobox' >
+      <div className='bp3-input-group jp-InputGroup'>
+        <input
+          {...getInputProps()}
+          className='bp3-input'
+          data-testid="DropdownCombobox-input"
+          placeholder={placeholder}
+        />
+      </div>
+      <ul
+        {...getMenuProps()}
+        data-testid="DropdownCombobox-ul"
+      >
+        {(isOpen || !selectedItem) &&
+          inputItems.map((item, index) => (
+            <li
+              style={{
+                backgroundColor: highlightedIndex === index ? '#bde4ff' : '',
+              }}
+              data-testid={`DropdownCombobox-li-${item}`}
+              key={`${item}${index}`}
+              {...getItemProps({
+                item,
+                index,
+              })}
+            >
+              {item}
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
+
+export default DropdownCombobox;
---
+++ jupyter-extension/src/components/DropdownCombobox/__tests__/DropdownCombobox.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+
+import DropdownCombobox, {DropdownComboboxProps} from '../DropdownCombobox';
+
+describe('DropdownCombobox', () => {
+  it('should render placeholder', async () => {
+    const props: DropdownComboboxProps = {
+      initialSelectedItem: null,
+      items: ['item1', 'item2', 'item3'],
+      placeholder: 'placeholder',
+      onSelectedItemChange: () => {},
+    }
+
+    const {getByTestId} = render(<DropdownCombobox {...props}/>,);
+
+    const input = getByTestId('DropdownCombobox-input');
+    expect(input).toHaveAttribute('placeholder', 'placeholder');
+  });
+
+  it('should select initial item', async () => {
+    const props: DropdownComboboxProps = {
+      initialSelectedItem: 'item1',
+      items: ['item1', 'item2', 'item3'],
+      onSelectedItemChange: () => {},
+    }
+
+    const {getByTestId} = render(<DropdownCombobox {...props}/>,);
+
+    const input = getByTestId('DropdownCombobox-input');
+    expect(input).toHaveValue('item1');
+    const ul = getByTestId('DropdownCombobox-ul');
+    expect(ul.children).toHaveLength(0);
+  });
+
+  it('should filter items based on input', async () => {
+    const props: DropdownComboboxProps = {
+      initialSelectedItem: null,
+      items: ['foo', 'bar'],
+      onSelectedItemChange: () => {},
+    }
+
+    const {getByTestId} = render(<DropdownCombobox {...props}/>,);
+
+    let ul = getByTestId('DropdownCombobox-ul');
+    expect(ul.children).toHaveLength(2);
+    let liFoo = getByTestId('DropdownCombobox-li-foo');
+    expect(liFoo).toHaveTextContent('foo');
+    let liBar = getByTestId('DropdownCombobox-li-bar');
+    expect(liBar).toHaveTextContent('bar');
+
+    const input = getByTestId('DropdownCombobox-input');
+    fireEvent.change(input, {target: {value: 'ba'}});
+
+    ul = getByTestId('DropdownCombobox-ul');
+    expect(ul.children).toHaveLength(1);
+    liBar = getByTestId('DropdownCombobox-li-bar');
+    expect(liBar).toHaveTextContent('bar');
+  });
+
+  // TODO: test for onSelectedItemChange trigger
+  // TODO: test for clearing out selectedItem on isOpen
+});
